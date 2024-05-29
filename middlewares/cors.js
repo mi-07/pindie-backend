deaf55f3fa87da68f742cc2156d4ba06064e527d
MishaBozhevskiy <misha.bozhev@gmail.com>
2024-05-29 17:33:25 +0300

    fix: add pindie domain to cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,6 +3,8 @@ const allowedCors = [
   "https://students-projects.ru",
   "http://localhost:3000",
   "http://localhost:3001",
+  "https://cat-lolofd-pindie-backend.nomorepartiesco.ru",
+  "http://cat-lolofd-pindie-backend.nomorepartiesco.ru",
 ];
 
 function cors(req, res, next) {
